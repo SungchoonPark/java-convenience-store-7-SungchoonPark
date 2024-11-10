2971d74866eee1bbef5c7dea888b24b64fb990f9
박성춘 <songchoon10@naver.com>
2024-11-11 03:23:24 +0900

    feat: 똑같은 상품을 입력했을 경우 예외 발생하도록 로직 구현 완료
    - docs: '똑같은 상품을 입력했을 경우 ex) `[사이다-3],[사이다-2]`' 항목 완료
    - docs: '공백 입력' 항목 완료

--- src/main/java/store/model/TemporaryPurchaseList.java
+++ src/main/java/store/model/TemporaryPurchaseList.java
@@ -1,7 +1,11 @@
 package store.model;
 
+import store.exception.ExceptionMessage;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TemporaryPurchaseList {
     private static final String PURCHASE_DELIMITER = ",";
@@ -10,10 +14,20 @@ public class TemporaryPurchaseList {
 
     public TemporaryPurchaseList(String purchase) {
         purchaseItems = new ArrayList<>();
+        Set<String> uniqueProductNames = new HashSet<>();
         String[] parsingItems = purchase.split(PURCHASE_DELIMITER);
         for (String item : parsingItems) {
-            this.purchaseItems.add(new TemporaryPurchaseInfo(item));
+            TemporaryPurchaseInfo temporaryPurchaseInfo = new TemporaryPurchaseInfo(item);
+            checkProductNameIsDuple(uniqueProductNames, temporaryPurchaseInfo.getProductName());
+            this.purchaseItems.add(temporaryPurchaseInfo);
         }
+    }
+
+    private void checkProductNameIsDuple(Set<String> uniqueNames, String productName) {
+        if (uniqueNames.contains(productName)) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT.getMessage());
+        }
+        uniqueNames.add(productName);
     }
 
     public List<TemporaryPurchaseInfo> getPurchaseItems() {

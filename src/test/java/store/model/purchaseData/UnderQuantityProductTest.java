d1b92bb87b56fcc067e9316f83f9fc3babc0063d
박성춘 <songchoon10@naver.com>
2024-11-11 14:38:41 +0900

    test: 무료로 받을 수 있는 수량이 있는 경우 사용자가 이를 원할시 구매 수량 증가 테스트 완료

---
+++ src/test/java/store/model/purchaseData/UnderQuantityProductTest.java
@@ -0,0 +1,39 @@
+package store.model.purchaseData;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.model.storeData.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+class UnderQuantityProductTest {
+
+    @DisplayName("무료로 받을 수 있는 수량이 있는 경우 구매 수량 증가")
+    @Test
+    void 무료로_받을수_있는_수량이_있는_경우_구매수량_증가() {
+        // given
+        Product purchaseProduct = new Product(
+                new ProductInfo("사이다", 3000, 10),
+                new ProductPromotion(List.of(
+                        new PromotionInfo(
+                                new Promotion("1+1", 1, 1, LocalDate.of(2024, 1, 1), LocalDate.of(2024, 12, 31)),
+                                10
+                        )
+                )
+                )
+        );
+        UnderQuantityProduct underQuantityProduct = new UnderQuantityProduct(purchaseProduct, 1);
+        long expectedPurchaseQuantity = 2;
+
+        // when
+        underQuantityProduct.updatePurchaseQuantity();
+        long actualPurchaseQuantity = underQuantityProduct.getPurchaseQuantity();
+
+        // then
+        Assertions.assertThat(actualPurchaseQuantity).isEqualTo(expectedPurchaseQuantity);
+    }
+
+}
